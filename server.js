f40978b4ab1e0e18f7b246a46d9b7eb279a1f9e2
alexkey89 <alexkey89@gmail.com>
2017-03-20 00:00:51 +0200

    migrated to es6

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
 server.listen(process.env.PORT || 3000);
 console.log('server running...')
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/index.html')
 })
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', (socket)=>{
   connections.push(socket);
   console.log('connected: %s sockets connected', connections.length)
 
   //disconnect
-  socket.on('disconnect', function(data){
-    //if(!socket.username) return;
+  socket.on('disconnect', (data)=>{
+    if(!socket.username) return;
     users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
@@ -26,13 +26,12 @@ io.sockets.on('connection', function(socket){
   })
 
   //send Message
-  socket.on('send message', function(data){
-    //console.log(data)
+  socket.on('send message', (data)=>{
     io.sockets.emit('new message', {msg: data, user: socket.username});
   })
 
   //new user
-  socket.on('new user', function(data, callback){
+  socket.on('new user', (data, callback)=>{
     callback(true);
     socket.username = data;
     users.push(socket.username)
